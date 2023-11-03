05b0b0e85b0df35106fbc47ffc145f0bdfef57b6
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2023-11-03 17:05:36 +0900

    appbarのコンポーネント

---
+++ lib/components/original_app_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:ishibashi/style/colors.dart';
+
+class OriginalAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const OriginalAppBar({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) => AppBar(
+      backgroundColor: Colors.white,
+      centerTitle: false,
+      title: Row(
+        mainAxisAlignment:MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 13, bottom: 14),
+            child: SizedBox(
+                height: 60, child: Image.asset('assets/images/iconKari.png'),),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20, bottom: 30),
+            child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: ColorName.greyBase,
+                  size: 60,
+                )),
+          )
+        ],
+      ),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(43.5),
+        ),
+      ));
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
