f9500af00353b6a25e979dea0d280b69d2a1e87a
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2023-11-27 10:46:11 +0900

    appbarの調整

--- lib/components/original_app_bar.dart
+++ lib/components/original_app_bar.dart
@@ -4,43 +4,36 @@ import 'package:ishibashi/screens/search_confirm/page/search_confirm.dart';
 import 'package:ishibashi/style/colors.dart';
 
 class OriginalAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const OriginalAppBar({Key? key}) : super(key: key);
+  const OriginalAppBar({super.key});
+
   @override
   Widget build(BuildContext context) => AppBar(
-      iconTheme: const IconThemeData(color: ColorName.greyBase),
-      backgroundColor: Colors.white,
-      centerTitle: false,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 13, bottom: 14),
-            child: SizedBox(
-              height: 60,
-              child: Image.asset('assets/images/iconKari.png'),
+        iconTheme: const IconThemeData(color: ColorName.greyBase),
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SearchConfirmPage(),
+                ),
+              );
+            },
+            icon: const FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+              color: ColorName.greyBase,
+              size: 30,
             ),
           ),
-          IconButton(
-              onPressed: () async {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SearchConfirmPage(),
-                  ),
-                );
-              },
-              icon: const FaIcon(
-                FontAwesomeIcons.magnifyingGlass,
-                color: ColorName.greyBase,
-                size: 30,
-              ))
         ],
-      ),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(43.5),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(43.5),
+          ),
         ),
-      ));
+      );
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
