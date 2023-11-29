16dabfda1013b9822b2b18af4fce2c85ebe2ce9b
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2023-11-29 14:38:20 +0900

    keywordコンポーネント化

--- lib/screens/search/keyword_search/page/keyword_search.dart
+++ lib/screens/search/keyword_search/page/keyword_search.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ishibashi/components/original_app_bar.dart';
-import 'package:ishibashi/components/store_list.dart';
+import 'package:ishibashi/screens/search/keyword_search/components/keyword_search_result_list.dart';
+import 'package:ishibashi/screens/search/keyword_search/components/keyword_search_text_field.dart';
 import 'package:ishibashi/screens/search/keyword_search/view_model.dart';
 
 class KeywordSearchPage extends ConsumerWidget {
@@ -12,48 +13,12 @@ class KeywordSearchPage extends ConsumerWidget {
     final state = ref.watch(keywordSearchViewModelProvider);
 
     return state.when(
-      data: (data) => Scaffold(
-        appBar: const OriginalAppBar(),
+      data: (data) => const Scaffold(
+        appBar: OriginalAppBar(),
         body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 12,
-                horizontal: 36,
-              ),
-              child: TextField(
-                controller: data.searchWordController,
-                style: const TextStyle(fontSize: 16),
-                decoration: const InputDecoration(
-                  hintText: '店名、カテゴリーなどで検索',
-                ),
-                onChanged: (value) async {
-                  await ref
-                      .read(keywordSearchViewModelProvider.notifier)
-                      .searchKeyword(data.searchWordController.text);
-                },
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: data.searchResultStoreList.length,
-                itemBuilder: (context, index) {
-                  final store = data.searchResultStoreList[index];
-                  return StoreList(
-                    onTap: () async {
-                      await ref
-                          .read(keywordSearchViewModelProvider.notifier)
-                          .navigateToStorePage(context, store.documentId);
-                    },
-                    name: store.storeName,
-                    imageUrl: store.storePhotoUrl,
-                    tags: store.tags,
-                    openTime: store.openTime,
-                    closeTime: store.closeTime,
-                  );
-                },
-              ),
-            ),
+            KeywordSearchTextField(),
+            KeywordSearchResultList(),
           ],
         ),
       ),
