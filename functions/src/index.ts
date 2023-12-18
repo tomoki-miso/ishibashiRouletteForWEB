521d66209095ca7023cb281c5734e89519886fb7
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2023-12-18 21:55:19 +0900

    function 完成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,37 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const firestore = admin.firestore();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const updateCouponStatus = functions.pubsub
+  .schedule("every day 17:35")
+  .timeZone("Asia/Tokyo") // タイムゾーンを指定
+  .onRun(async (context) => {
+    try {
+      const userInfosSnapshot = await firestore.collection("user_info").get();
+
+      const batch = firestore.batch();
+
+      userInfosSnapshot.forEach((doc) => {
+        const userInfo = doc.data();
+
+        // 必要な条件をここでチェックして、isCanGetCouponがfalseの場合にtrueに更新
+        if (
+          !("isCanGetCoupon" in userInfo) ||
+          userInfo.isCanGetCoupon === false
+        ) {
+          const userRef = firestore.collection("user_info").doc(doc.id);
+          batch.update(userRef, {isCanGetCoupon: true});
+        }
+      });
+
+      await batch.commit();
+      console.log("Updated coupon statuses successfully.");
+      return null;
+    } catch (error) {
+      console.error("Error updating coupon statuses:", error);
+      return null;
+    }
+  });
