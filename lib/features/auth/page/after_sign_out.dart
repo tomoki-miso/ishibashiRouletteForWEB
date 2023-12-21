e262a0bde44abbebde1ca77921ba0249e3e10a04
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2023-12-22 00:21:40 +0900

    wilPop 修正

--- lib/features/auth/page/after_sign_out.dart
+++ lib/features/auth/page/after_sign_out.dart
@@ -4,9 +4,9 @@ class AfterSignOutPage extends StatelessWidget {
   const AfterSignOutPage({super.key});
 
   @override
-  Widget build(BuildContext context) => WillPopScope(
-        onWillPop: () async => false,
-        child: const Scaffold(
+  Widget build(BuildContext context) => const PopScope(
+        canPop: false,
+        child: Scaffold(
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -15,7 +15,7 @@ class AfterSignOutPage extends StatelessWidget {
                   'ログアウトしました。\nアプリを一旦閉じてください。',
                   style: TextStyle(fontSize: 20),
                   textAlign: TextAlign.center,
-                )
+                ),
               ],
             ),
           ),
